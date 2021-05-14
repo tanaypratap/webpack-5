2780a237745a5ecf401bd529202332dc330eac86
Tanay Pratap <tapratap@microsoft.com>
2021-05-14 07:28:00 +0530

    fix: webpack config to be used

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
 const webpackConfig = {
-    entry: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "src")
-    },
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    }
-}
+	entry: path.resolve(__dirname, "src", "index.js"),
+    
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist")
+	}
+};
+
+ module.exports = webpackConfig
