7a0a418e9a62cda4301c6bfa204ca54102d98332
Tanay Pratap <tapratap@microsoft.com>
2021-05-15 13:12:00 +0530

    code splitting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import _ from "lodash";
 import "./App.css";
 
 const App = ({ name }) => {
-    console.log('just testing hash..')
     return `
     <h2> ${ _.join(["Welcome,", name], " ")} </h2>
     <p> Build apps with plain JS but with modern tooling  </p>
