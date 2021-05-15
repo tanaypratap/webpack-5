36588370f951caaa279ab5d8cc10751838c078a6
Tanay Pratap <tapratap@microsoft.com>
2021-05-15 21:00:40 +0530

    latest

---
+++ README.md
@@ -0,0 +1,27 @@
+# vanilla starter with modern tools
+
+## getting started
+ npx degit tanaypratap/vanillaStarter expense-app
+ cd expense-app
+ yarn 
+ yarn start 
+
+
+## features
+
+- import/export 
+- minif
+- hashing
+- css loader
+- babel support
+- inline image support
+
+## setting up on Netlify
+
+
+## examples
+
+### lazy load
+
+
+## more configurations
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import App from "./App";
 import Logo from "./logo.svg";
 
-const root = document.getElementById("root");
+let root;
 
-console.log("testing...")
-root.innerHTML = `
-<img src="${Logo}" alt="builder icon" />
-<h1> learning web tooling today! </h1>
-${App({ name: "Tanay" })}
+function init() {
+	root = document.getElementById("root");
+	root.innerHTML = `
+            <img src="${Logo}" alt="builder icon" />
+            <h1> learning web tooling today! </h1>
+            <button id="about">About </button>
+            <button id="contact">Contact </button> <!-- homework -->
+            <div id="route"></div>
+            
 `;
+}
+
+init();
+
+const button = document.querySelector("#about");
+button.addEventListener("click", loadAbout)
+
+
+function loadAbout() {
+    import("./App").then(({ default: App }) => {
+        const about = App({ name: "Tanay" });
+
+        document.querySelector("#route").innerHTML = about;
+    })
+}
+
