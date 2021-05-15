3ab12c0ca706a522e81ac837ff529b915d31f6a9
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2021-05-15 20:01:42 +0530

    added htmlwebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,45 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
 	entry: path.resolve(__dirname, "src", "index.js"),
 
 	output: {
 		filename: "main.js",
-		path: path.resolve(__dirname, "dist")
+		path: path.resolve(__dirname, "dist"),
+		clean: true
 	},
-    //added babel to webpack know have .concat instead of arrow func in main.js
-    module:{
-        rules: [
-            {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader:"babel-loader",
-                options:{
-                    presets: ["@babel/preset-env"]
-                }
-            }
-        },
-        //telling webpack to test css as of now it don't know css
-            {
-                test: /\.css$/i,
-                use:["style-loader", "css-loader"]
-            },
-            //adding svg to webpack 
-            {
-                test:/\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset" 
-            }
-        ]
-    },
-    mode: "production",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset"
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "vanillaJS app",
+			template: path.resolve(__dirname, "src", "index.html")
+		})
+	],
+
+	mode: "production"
 };
 
 module.exports = webpackConfig;
