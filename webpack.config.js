31d37236a9b57e9e9a562f942e3f7d144b5791da
Tanay Pratap <tapratap@microsoft.com>
2021-05-15 16:32:31 +0530

    htmlwebpackplugin working

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const webpackConfig = {
 	entry: path.resolve(__dirname, "src", "index.js"),
 
 	output: {
 		filename: "main.js",
-		path: path.resolve(__dirname, "dist")
+		path: path.resolve(__dirname, "dist"),
+		clean: true
 	},
 	module: {
 		rules: [
@@ -29,6 +31,13 @@ const webpackConfig = {
 			}
 		]
 	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "vanillaJS app",
+			template: path.resolve(__dirname, "src", "index.html")
+		})
+	],
 
 	mode: "production"
 };
