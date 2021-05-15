19f89c55ec2cfa40828becb08ff26e5aee9a22dd
Tanay Pratap <tapratap@microsoft.com>
2021-05-15 16:43:29 +0530

    adding code splitting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import _ from "lodash";
 import "./App.css";
 
 const App = ({ name }) => {
 	return `
-    <h2> Welcome, ${name} </h2>
+    <h2> ${_.join(["Welcome,", name], " ")} </h2>
+    <p> Build apps with plain JS but with modern tooling  </p>
     `;
 };
 
