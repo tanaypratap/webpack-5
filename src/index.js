dcf0d2fc22f65324c53500fc5afa19e0688b85b7
Tanay Pratap <tapratap@microsoft.com>
2021-05-15 19:30:12 +0530

    img asset

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import App from "./App";
+import Logo from "./logo.svg";
 
 const root = document.getElementById("root");
 root.innerHTML = `
+<img src="${Logo}" alt="builder icon" />
 <h1> learning web tooling today </h1>
 ${App({ name: "Tanay" })}
 `;
