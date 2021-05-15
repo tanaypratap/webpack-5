760940f6fdb1bb613ad8a501dba512efdb90a06b
Tanay Pratap <tapratap@microsoft.com>
2021-05-15 16:05:47 +0530

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,20 @@ const webpackConfig = {
 		filename: "main.js",
 		path: path.resolve(__dirname, "dist")
 	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env"]
+					}
+				}
+			}
+		]
+	},
 
 	mode: "production"
 };
